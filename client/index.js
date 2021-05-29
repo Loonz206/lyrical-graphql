58f49c43342967bee1149b0e2bb6be63cbd08365
Lenny Peters <mr.lennypeters@gmail.com>
2021-05-29 08:25:22 -0700

    refactor: 💡 adding weird new router5

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "react-apollo";
 import ErrorBoundary from "./components/ErrorBoundry";
@@ -7,14 +8,18 @@ const App = lazy(() => import("./components/App"));
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache: cache,
-  uri: "http://localhost:4000/",
+  uri: "http://localhost:4000/graphql",
 });
 
 ReactDOM.render(
-  <ErrorBoundary>
+  <ErrorBoundary fallback={"An error has occurred"}>
     <Suspense fallback={<div>Loading...</div>}>
       <ApolloProvider client={client}>
-        <App />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={App}></Route>
+          </Switch>
+        </BrowserRouter>
       </ApolloProvider>
     </Suspense>
   </ErrorBoundary>,
