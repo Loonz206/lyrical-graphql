398bd0886d5587a1cc117a0e7e3b6549e92a55b5
Lenny Peters <mr.lennypeters@gmail.com>
2022-02-15 21:21:46 -0800

    refactor: 💡 updated song.js to see if addressing codesmell

--- server/models/song.js
+++ server/models/song.js
@@ -21,9 +21,7 @@ SongSchema.statics.addLyric = function (id, content) {
   return this.findById(id).then((song) => {
     const lyric = new Lyric({ content, song });
     song.lyrics.push(lyric.id);
-    return Promise.all([lyric.save(), song.save()]).then(
-      ([lyric, song]) => song
-    );
+    return Promise.all([lyric.save(), song.save()]).then(([song]) => song);
   });
 };
 
